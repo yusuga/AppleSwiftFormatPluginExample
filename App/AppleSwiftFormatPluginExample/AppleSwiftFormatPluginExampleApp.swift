73fbf1860c12e3ac761e699789dd690733e7729e
yusuga <dev@yusuga.com>
2023-12-03 12:53:38 +0900

    feat: add AppleSwiftFormatPluginExample.xcodeproj

---
+++ App/AppleSwiftFormatPluginExample/AppleSwiftFormatPluginExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleSwiftFormatPluginExampleApp.swift
+//  AppleSwiftFormatPluginExample
+//
+//  Created by yusuga on 2023/12/03.
+//
+
+import SwiftUI
+
+@main
+struct AppleSwiftFormatPluginExampleApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
---
+++ App/AppleSwiftFormatPluginExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppleSwiftFormatPluginExample
+//
+//  Created by yusuga on 2023/12/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundStyle(.tint)
+      Text("Hello, world!")
+    }
+    .padding()
+  }
+}
+
+#Preview {
+  ContentView()
+}
